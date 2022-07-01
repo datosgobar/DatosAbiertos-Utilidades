bdc2bed316e4fe8c4519919ff898f5ac69e28001
Pavlo AE <pavloae@gmail.com>
2022-07-01 11:51:14 -0300

    fix: catalog_restore endpoint
    Arregla parámetro file
    Cambia la función a async

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tempfile
 
 import uvicorn
-from fastapi import FastAPI, UploadFile, Query
+from fastapi import FastAPI, UploadFile, Query, File
 from fastapi.responses import RedirectResponse
 
 from portal_andino.update import catalog_update
@@ -33,15 +33,15 @@ def root():
     description="Restaura los datasets de un catálogo original al portal pasado por parámetro. Si hay temas presentes "
                 "en el DataJson que no están en el portal de CKAN, los genera."
 )
-def catalog_restore(
-        file: UploadFile = Query(description="El catálogo de origen que se restaura."),
+async def catalog_restore(
+        file: UploadFile = File(description="El catálogo de origen que se restaura."),
         origin_url: str = Query(description="La URL del portal CKAN de origen."),
         destination_url: str = Query(description="La URL del portal CKAN de destino."),
         apikey: str = Query(description="La apikey de un usuario con los permisos que le permitan crear o "
                                         "actualizar el dataset")
 ):
     with tempfile.NamedTemporaryFile() as catalog:
-        content = file.file.read()
+        content = await file.read()
         catalog.write(content)
         catalog.seek(0)
         pushed_datasets = catalog_update(catalog.name, origin_url, destination_url, apikey)
