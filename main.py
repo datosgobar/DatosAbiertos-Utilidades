4aaea03dbad8062af134e8651487f2f14d609f50
Pavlo AE <pavloae@gmail.com>
2022-07-07 11:47:22 -0300

    Rename catalog_restore

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uvicorn
 from fastapi import FastAPI, UploadFile, Query, File
 from fastapi.responses import RedirectResponse
 
-from portal_andino.update import catalog_update
+from portal_andino.update import catalog_restore
 from portal_andino.info import get_organizations
 
 app = FastAPI(
@@ -40,7 +40,7 @@ def organizations_portal(
 
 
 @app.post(
-    "/catalog/restore",
+    "/portal/catalog/restore",
     name="Restauración de catálogo",
     description="Restaura los datasets de un catálogo original al portal pasado por parámetro. Si hay temas presentes "
                 "en el DataJson que no están en el portal de CKAN, los genera."
@@ -56,7 +56,7 @@ async def catalog_restore(
         content = await file.read()
         catalog.write(content)
         catalog.seek(0)
-        pushed_datasets = catalog_update(catalog.name, origin_url, destination_url, apikey)
+        pushed_datasets = catalog_restore(catalog.name, origin_url, destination_url, apikey)
 
     return pushed_datasets
 
